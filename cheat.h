4e8355a30a0875c5036a3992e44242c3d24b6956
naturaldream <82487038+naturaldream@users.noreply.github.com>
2023-08-30 10:44:46 +0800

    没开源关键数据--泪心逗你呢
    故意的  防止狗逼崽子又去圈钱了 连单机游戏都圈的出来  真心要学逆向的去蓝云或者群里问源码  一般发在群文件

---
+++ cheat.h
@@ -0,0 +1,19 @@
+#pragma once
+//全局参数
+extern DWORD  coin;
+extern DWORD  thing;
+extern DWORD  cd;
+extern   DWORD module;     
+
+//局部函数吧 
+void  func1();
+void func2();
+void func3();
+void unfunc1();
+void unfunc2();
+void unfunc3();
+void  GetGame();
+
+void freezecoin();
+void releasecoin();
+
